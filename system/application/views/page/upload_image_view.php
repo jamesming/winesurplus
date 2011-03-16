51749c097d80e1e1f9eb19f6fd9618e16e215c50
James Ming <jamesming@gmail.com>
2011-03-16 14:54:54 -0700

    added

--- system/application/views/page/upload_image_view.php
+++ system/application/views/page/upload_image_view.php
@@ -1,6 +1,6 @@
 <script type="text/javascript" language="Javascript">
 
-							window.parent.open_jcrop();  //FOUND:line 98  header/wysiwyg_jquery_with_iframe.php
+							window.parent.open_jcrop();  //FOUND:line 88  header/wysiwyg_jquery_with_iframe.php
 							document.location.href='<?php echo base_url();    ?>index.php/page/upload_image_form/<?php echo $product_id;    ?>';
 		
 											
